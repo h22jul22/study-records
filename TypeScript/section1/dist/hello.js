32d9074c35e5e95ad1a8726d2803aca8f25130f6
h22jul22 <164333745+h22jul22@users.noreply.github.com>
2025-03-28 17:40:44 +0900

    타입스크립트 컴파일러 옵션 설정하기

---
+++ TypeScript/section1/dist/hello.js
@@ -0,0 +1,7 @@
+// 전역 모듈
+const a = 1;
+export {};
+// 해결 방법 1.
+// export { }
+// 해결 방법 2.
+// tsconfig.json "moduleDetection": "force"
---
+++ TypeScript/section1/src/hello.ts
@@ -0,0 +1,8 @@
+// 전역 모듈
+const a = 1;
+
+// 해결 방법 1.
+// export { }
+
+// 해결 방법 2.
+// tsconfig.json "moduleDetection": "force"
