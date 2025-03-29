baa5095a25c8b73936720174a0f747d79d631421
h22jul22 <164333745+h22jul22@users.noreply.github.com>
2025-03-29 13:52:05 +0900

    enum type

---
+++ TypeScript/section2/src/chapter5.ts
@@ -0,0 +1,34 @@
+// enum type
+// 여러가지 값들에 각각 이름을 부여하여 열거해두고 사용하는 타입
+
+enum Role {
+    ADMIN = 0,
+    USER = 1,
+    GUEST = 2,
+}
+
+enum Language {
+    korean = 'ko',
+    english = 'en',
+}
+
+const user1 = {
+    name: '이희주',
+    // role: 0 // 0 <- 관리자
+    role: Role.ADMIN,
+    language: Language.korean,
+};
+
+const user2 = {
+    name: '홍길동',
+    role: Role.USER, // 1 <- 일반 유저
+    language: Language.korean,
+};
+
+const user3 = {
+    name: '아무개',
+    role: Role.GUEST, // 2 <- 게스트
+    language: Language.korean,
+};
+
+console.log(user1, user2, user3);
